3327c9c107f334afe074835d44e72d5fcd8b76c6
Shiv Amin <shivamin187@gmail.com>
2026-04-03 22:10:33 -0400

    insights removed from backend

--- server/routes/insightsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticate } = require('../middleware/authMiddleware');
-const { getInsights } = require('../controllers/insightsController');
-
-router.get('/', authenticate, getInsights);
-
-module.exports = router;
